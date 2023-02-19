62a88e6e220cc53d43da73640c33cb1d55b0408e
Promiseejikeme <ejikemepromise1997@gmail.com>
2023-02-19 01:02:05 -0800

    all remaining task of 0x00. C - Hello, World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main- this is a description
+ * return ; always 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
