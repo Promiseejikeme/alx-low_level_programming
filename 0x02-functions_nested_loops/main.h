45d3d0e15053db60ae1b69e37a6cca0bf30be2e1
Promiseejikeme <ejikemepromise1997@gmail.com>
2023-02-21 19:27:18 -0800

    not sure what this is

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,14 +4,14 @@
  * Description: declare the function butdoes not call it
  * Return: always 0
  */
-void print_alphabet(void)
-{
-	char c = 'a';
-
-	while (c < 'z')
-	{
-		putchar(c);
-		c++;
-	}
-	putchar('\n');
-}
+void print_alphabet(void);
+int _putchar(char c);
+void print_alphabet_x10(void);
+int _islower(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
