4178f736e5963ff4c453b52c273ad86c2c62636b
Promiseejikeme <ejikemepromise1997@gmail.com>
2023-02-21 19:43:18 -0800

    fix made for betty styling

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,19 +1,20 @@
 #include "main.h"
 /**
  * main - Entry point
- * Description: function that prints x10 the alphabet
+ * Description: the functions prints x10 of the alphabet
  * Return: always 0
  */
-void print_alphabet_x10(void) 
+void print_alphabet_x10(void)
+/*Description: the function prints x10 off the alphabet */
 {
 	int i, j;
 
-    	for (i = 0; i < 10; i++) 
+	for (i = 0; i < 10; i++)
 	{
-	    for (j = 'a'; j <= 'z'; j++)
-	    {
-		_putchar(j);
-	    }	
-	_putchar('\n');
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		putchar('\n');
 	}
 }
