dca5229ca74831c7ffc4d7a459bbc0baec91b952
Promiseejikeme <ejikemepromise1997@gmail.com>
2023-02-24 15:14:45 -0800

    new commit

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,13 +7,14 @@ void print_line(int n)
 {
 	int i;
 
-	for (i = 0; i < n; n++)
+	if (n <= 0)
 	{
-		if (n <= 0)
-		{
-			_putchar('\n');
-			return;
-		}
+		_putchar('\n');
+		return;
+	}
+	for (i = 0; i < n; i++)
+	{
 		_putchar('_');
 	}
+	_putchar('\n');
 }
