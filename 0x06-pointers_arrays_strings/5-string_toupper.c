ec553b5fa02cad761281c0a1d536eabf30f399d6
Promiseejikeme <ejikemepromise1997@gmail.com>
2023-03-02 10:23:35 -0800

    new commit

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * string_toupper - changes string from lowercase to uppercase
+ * int - accepts any integer value
+ * char - accepts any character value
+ * @str[i]: represents an array of i entries
+ * Return: returns str
+ */
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+		i++;
+	}
+	return (str);
+}
