949221dd2b233a10c2ad636cea80c0e2bcbec731
Promiseejikeme <ejikemepromise1997@gmail.com>
2023-03-02 14:05:02 -0800

    a fix

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -4,30 +4,23 @@
 #include <stddef.h>
 #include <ctype.h>
 /**
- * cap_string - a function that capitalizes all words of a string
- * int - accepts any integer value
- * char - accept any character variable
- * @sep[]: an array of special characters
- * @str[i]: an array the takes in i variables
- * toupper: converts string to uppercase
- * tolower: converts string to lowercase
- * Return: returns str
+ * cap_string - Capitalizes all words of a string.
+ * @s: The string to be modified.
+ * Return: A pointer to the modified string.
  */
-char *cap_string(char *str)
+char *cap_string(char *s)
 {
 	int i;
-	char sep[] = " \t\n,;.!?\"(){}";
-
-	for (i = 0; str[i] != '\0'; i++)
+	
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (i == 0 || str[i - 1] == '\0' || strchr(sep, str[i - 1]) != NULL)
+		if (s[i] >= 'a' && s[i] <= 'z')
 		{
-			str[i] = toupper(str[i]);
-		}
-		else
-		{
-			str[i] = tolower(str[i]);
+			if (i == 0 || s[i - 1] == ' ' || s[i - 1] == '\t' || s[i - 1] == '\n' || s[i - 1] == ',' || s[i - 1] == ';' || s[i - 1] == '.' || s[i - 1] == '!' || s[i - 1] == '?' || s[i - 1] == '"' || s[i - 1] == '(' || s[i - 1] == ')' || s[i - 1] == '{' || s[i - 1] == '}')
+			{
+				s[i] -= 32;
+			}
 		}
 	}
-	return (str);
+	return (s);
 }
