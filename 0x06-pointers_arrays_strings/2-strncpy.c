bcd79d02cb722138cd5ebae80dad23e7c8046819
Promiseejikeme <ejikemepromise1997@gmail.com>
2023-03-02 09:15:19 -0800

    new commit

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strncpy - copies a string to a destination
+ * int - accepts any integer value
+ * @dest: destination location
+ * @src: source location
+ * @n: maximum chararcters
+ * Return: returns dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
+}
