5f8cef0a5d97331afd932c754a08db60b3065db7
Promiseejikeme <ejikemepromise1997@gmail.com>
2023-03-02 16:14:51 -0800

    nw commit

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * rot13 - replaces a letter with the 13th letter after it
+ * @p: a pointer to str
+ * @str: string holder
+ */
+char *rot13(char *str)
+{
+	char *p = str;
+	int i;
+
+	while (*p)
+	{
+		for (i = 0; (i < 13 && (*p >= 'a' && *p <= 'z')) || (i < 13 && (*p >= 'A' && *p <= 'Z')); i++)
+		{
+			if ((*p >= 'a' && *p == 'm') || (*p >= 'A' && *p <= 'M'))
+				*p += 13;
+			else if ((*p >= 'n' && *p <= 'z') || (*p >= 'N' && *p <= 'Z'))
+				*p -= 13;
+		}
+		p++;
+	}
+	return (str);
+}
